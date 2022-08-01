6353a71d1597156ff39e8c66421b6411cffd7475
VladimirJG <vladimi85@gmail.com>
2022-08-01 08:15:13 +0300

    23 урок

---
+++ JS-1.js
@@ -0,0 +1,50 @@
+/* console.log('- Did Joffrey agree?\n' +
+    '- He did. He also said "I love using \\n".'); */
+
+/* const eurosCount = 100;
+const dollarCount = (eurosCount * 1.25);
+console.log(dollarCount);
+console.log(dollarCount * 60); */
+
+
+/* const bornKing = 'The king was born in hungry year';
+const whereBorn = 'in little room inside city_of_pook';
+console.log(`${bornKing}, ${whereBorn}` + ' 23.09.') */
+
+
+/* const magic = '\nyou'
+console.log(magic[1]); */
+
+/* console.log(-0.102 + -0.202); */
+
+/* console.log(Math.sqrt(1, 2)) */
+
+/* const name = 'JavaScript';
+// Используется интерполяция
+console.log(`Последний символ: ${name[length(name) - 1]}`);
+// 'Последний символ: t' */
+
+
+/* console.log(Math.round(Math.random() * 10)); */
+
+/* console.log(name[name.length]) */
+
+/* let firstName = '  Grigor   \n';
+console.log(firstName.trim()) */
+
+/* const name1 = 'Tirion';
+console.log(name1.length.toString());
+
+const name = 'Tirion';
+const len = name.length;
+console.log(len.toString()); */
+
+/* const name = 'Tirion';
+console.log(name.toUpperCase().toLowerCase()); */
+
+/* const name = 'Tirion';
+console.log(name.toUpperCase().toLowerCase().length.toString().length); */
+
+
+const text = 'When \t\n you play a \t\n game of thrones you win or you die.';
+console.log(text.slice(5, 15).trim().length)
