0fec7d7bd406fc8135581a6dc9554d61cd3bfa09
VladimirJG <vladimi85@gmail.com>
2022-08-01 10:51:34 +0300

    выполнение задания 23 (printMotto())

---
+++ с 23.js
@@ -0,0 +1,11 @@
+//Реализуйте функцию с именем printMotto(), которая выведет на экран фразу Winter is coming.
+
+//printMotto(); // => "Winter is coming"
+
+
+/* const printMotto = () => {
+    const text = 'Winter is coming';
+    console.log(text);
+}
+
+printMotto() */
