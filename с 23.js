98e231f48c8be387274c39a2b285002e01d65e0d
VladimirJG <vladimi85@gmail.com>
2022-08-02 07:55:14 +0300

    25 создание функции (два значения) застрял - сделал.

--- с 23.js
+++ с 23.js
@@ -52,3 +52,33 @@ const hurray = sayHurrayThreeTimes()
 
 console.log(hurray)
  */
+
+// Варивнты использования GetlastChar 13
+/* const getLastChar = (str) => {
+    return str[str.length - 1];
+};
+console.log(getLastChar('hexlet'));
+
+const text = 'Winter is coming';
+console.log(getLastChar(text)); */
+
+// 13
+
+
+/* Реализуйте функцию truncate(), которая обрезает переданную строку до указанного количества символов, добавляет в конце многоточие и возвращает получившуюся строку. Подобная логика часто используется на сайтах, чтобы отобразить длинный текст в сокращенном виде.
+
+Функция принимает два параметра:
+
+Строка, которую нужно обрезать
+Число символов, которые нужно оставить */
+
+
+
+const truncate = (text, length) => {
+
+    const index = 0;
+    return (text.slice(0, length) + '...');
+
+};
+
+console.log(truncate('Windows', 3));
