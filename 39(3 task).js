ea53dcf697aac33279a17e8a0d6fb234b739b867
VladimirJG <vladimi85@gmail.com>
2022-08-09 11:42:46 +0300

    39 task solved (no)

--- 39(3 task).js
+++ 39(3 task).js
@@ -3,7 +3,7 @@
 
 
 
-const getHalfString = (str) => {
+/* const getHalfString = (str) => {
     let result = '';
     for (let i = 0; i < str.length / 2; i += 1) {
         result += str[i];
@@ -11,4 +11,22 @@ const getHalfString = (str) => {
     return result;
 };
 
-console.log(getHalfString("12345")); // 123
+console.log(getHalfString("12345")); // 123 */
+
+//===========================================================================
+
+
+// Сэмвелл обнаружил, что его сообщения перехватываются в замке «Близнецы» и там читаются. 
+//Из-за этого их атаки перестали быть внезапными. Немного подумав, он разработал программу, 
+//которая бы шифровала сообщения по следующему алгоритму. 
+//Она бы брала текст и переставляла в нем каждые два подряд идущих символа.
+
+const encrypt = (str) => {
+    let result = '';
+    for (let i = 0; i < str.length; i += 2) {
+        const nextSymbol = str[i + 1] || '';
+        result = `${result}${nextSymbol}${str[i]}`;
+    }
+
+    return result;
+};
